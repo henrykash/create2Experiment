b0a39e897c05dbce8644fad2614eec03289fb644
henrykash <henrykariuki003@gmail.com>
2023-03-24 16:57:40 +0300

    refactored vaultDeploy.ts

--- scripts/vaultDeploy.ts
+++ scripts/vaultDeploy.ts
@@ -1,13 +1,26 @@
 import { ethers } from "hardhat"
+import { create2Address, encoder } from "../src/utils/utils";
+const { bytecode } = require("../artifacts/contracts/Vault.sol/Vault.json");
+
+
+
 
 const main = async () => {
   const unlockTime = '1679732469' // unlock time must be > deployment time.
 
-  const Vault = await ethers.getContractFactory('Vault')
-  const vault = await Vault.deploy(unlockTime)
+  const factoryAddr = "0x204d8488a6999B5F11eA0c30E6B4277ADe97f2A6"; //replace with your factory address
+  const saltHex = ethers.utils.id("1234");
+  const initCode = bytecode + encoder(["uint"], [unlockTime]);
 
-  await vault.deployed()
-  console.log('Vault deployed to:', vault.address)
+  const create2Addr = create2Address(factoryAddr, saltHex, initCode);
+  console.log("precomputed address:", create2Addr);
+
+  const Factory = await ethers.getContractFactory("DeterministicDeployFactory");
+  const factory = await Factory.attach(factoryAddr);
+
+  const lockDeploy = await factory.deploy(initCode, saltHex);
+  const txReceipt = await lockDeploy.wait();
+  console.log("Deployed to:", txReceipt.events[0].args[0]);
 }
 
 main()
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,16 +1,14 @@
 import { ethers } from "ethers";
 
-const encoder = (types: any, values: any) => {
+export const encoder = (types: any, values: any) => {
     const abiCoder = ethers.utils.defaultAbiCoder;
     const encodedParams = abiCoder.encode(types, values);
     return encodedParams.slice(2);
 };
 
-const create2Address = (factoryAddress: any, saltHex: any, initCode: any) => {
+export const create2Address = (factoryAddress: any, saltHex: any, initCode: any) => {
     const create2Addr = ethers.utils.getCreate2Address(factoryAddress, saltHex, ethers.utils.keccak256(initCode));
     return create2Addr;
 
 }
 
-exports.encoder = encoder;
-exports.create2Address = create2Address;
